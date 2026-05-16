92187db1ad939761533c8887819d6c015870ebe7
folin03 <martinfolke.uk@gmail.com>
2026-05-16 06:26:40 +0100

    chores: clean the project from initial screen

--- App.tsx
+++ App.tsx
@@ -5,11 +5,9 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -24,14 +22,10 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <></>
     </View>
   );
 }
